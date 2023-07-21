146a8970c18223fecee7c22194be4c90fa07a716
twave <1340281297@qq.com>
2023-07-21 09:34:03 +0800

    添加了下载文件Controller

---
+++ src/main/java/com/twave/myapplication/Controller/FileController.java
@@ -0,0 +1,43 @@
+package com.twave.myapplication.Controller;
+
+import com.twave.myapplication.Service.Impl.FileServiceImpl;
+import com.twave.myapplication.Util.JSONResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 上传文件的Controller
+ *
+ * @author : twave
+ * @date : 2023/7/14 16:54
+ */
+@CrossOrigin
+@Controller
+@RequestMapping("file")
+public class FileController extends BaseController {
+    @Autowired
+    FileServiceImpl fileService;
+
+    @PostMapping("/upload")
+    public JSONResult<String> saveFile(@RequestParam("file") MultipartFile file) {
+        // 调用业务层的方法，将文件保存到服务器设定的位置
+        fileService.uploadFile(file);
+        // 如果上面的方法没有抛出异常，则执行成功，可以返回成功的状态码200和文本信息
+        return new JSONResult<>(REQUEST_SUCCESS, "文件上传成功");
+    }
+
+    // 引入commons-io或hutool方便流操作
+    @GetMapping("/getfile")
+    // SpringMVC会自动注入参数response-
+    public void sendFile(HttpServletResponse response, @RequestParam String fileName) {
+        // 直接调用业务层的方法，将文件写入到response中
+        fileService.downloadFile(response, fileName);
+    }
+}
