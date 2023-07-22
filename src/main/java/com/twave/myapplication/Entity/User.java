d65104028329b0a5b913d22b8e9456e7aa12a198
twaveLab <1340281297@qq.com>
2023-07-22 17:51:24 +0800

    用户实体类

---
+++ src/main/java/com/twave/myapplication/Entity/User.java
@@ -0,0 +1,50 @@
+package com.twave.myapplication.Entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * @author TWAVE
+ * @TableName tb_user
+ */
+@Data
+public class User implements Serializable {
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * 手机号码
+     */
+    private String phone;
+
+    /**
+     * 密码，加密存储
+     */
+    private String password;
+
+    /**
+     * 昵称，默认是用户id
+     */
+    private String nickName;
+
+    /**
+     * 人物头像
+     */
+    private String icon;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+}
